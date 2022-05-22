9e13800305f472282bfec3eebbcc160b008539c6
7Assassins <abhinav.20m0141045@abes.ac.in>
2022-05-23 01:15:56 +0530

    package vvhnages

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello! It is running12345')
-})
+  res.send('Hello! It is running12345');
+});
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+module.exports = app;
